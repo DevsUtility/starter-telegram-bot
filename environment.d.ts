46961e899d113bb13287aa878404cad76cdf2314
DevsUtility <125385070+DevsUtility@users.noreply.github.com>
2023-02-19 15:15:20 +0100

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      TELEGRAM_TOKEN: string;
+      TELEGRAM_TOKEN: "5938478522:AAFOB-WjyukvFisz90vHoWoowHMF38vUDAM";
       NODE_ENV?: "development" | "production";
       PORT?: number;
     }
